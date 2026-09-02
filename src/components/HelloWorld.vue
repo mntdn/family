b8d5e82550077de74fae295fba0fdf13e45b7611
Matthieu <matmon@gmail.com>
2026-09-02 09:00:54 +0100

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
+
+defineProps<{
+  msg: string
+}>()
+
+const s = useCounterStore()
+const { count } = storeToRefs(s)
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">
+      {{ msg }}{{ count }} <button @click="s.increment()">INC</button>
+      <el-button>I am ElButton</el-button>
+    </h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MealPlanner from '../components/MealPlanner.vue'
+</script>
+
+<template>
+  <main>
+    <MealPlanner />
+  </main>
+</template>
